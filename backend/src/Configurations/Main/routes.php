323a6820cd3d9653dee56f0e3d3af5de8ea04df5
Alexey Shmelev <alexsey_89@bk.ru>
2024-01-06 13:11:13 +0300

    added home route

--- backend/src/Configurations/Main/routes.php
+++ backend/src/Configurations/Main/routes.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Action\HomeAction;
 use Slim\App;
 
-return static function (App $application): void {};
+return static function (App $application): void {
+    $application->get('/', HomeAction::class);
+};
