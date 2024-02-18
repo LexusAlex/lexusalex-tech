7540ba3691beb236d1c6f0b1e3c032112a4f7178
Alexey Shmelev <alexsey_89@bk.ru>
2024-02-18 14:27:38 +0300

    added clear empty input middleware

--- backend/src/Configurations/Main/middleware.php
+++ backend/src/Configurations/Main/middleware.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\ClearEmptyInput;
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
 
 return static function (App $application): void {
     // Выполнение action
+    $application->add(ClearEmptyInput::class);
     $application->addBodyParsingMiddleware();
     $application->add(ErrorMiddleware::class);
 };
---
+++ backend/src/Http/Test/Middleware/ClearEmptyInputTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Test\Middleware;
+
+use App\Http\Middleware\ClearEmptyInput;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Factory\ResponseFactory;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Factory\UploadedFileFactory;
+
+final class ClearEmptyInputTest extends TestCase
+{
+    public function testParsedBody(): ResponseInterface
+    {
+        $middleware = new ClearEmptyInput();
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://test')
+            ->withParsedBody([
+                'null' => null,
+                'space' => ' ',
+                'string' => 'String ',
+                'stringru' => ' Строка ',
+                'int' => 42,
+                'nested' => [
+                    'array' => [],
+                    'null' => null,
+                    'space' => ' ',
+                    'name' => '     Name  ',
+                ],
+            ]);
+
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->expects(self::once())->method('handle')
+            ->willReturnCallback(static function (ServerRequestInterface $request): ResponseInterface {
+                self::assertEquals([
+                    'null' => null,
+                    'space' => '',
+                    'string' => 'String',
+                    'stringru' => 'Строка',
+                    'int' => 42,
+                    'nested' => [
+                        'array' => [],
+                        'null' => null,
+                        'space' => '',
+                        'name' => 'Name',
+                    ],
+                ], $request->getParsedBody());
+                return (new ResponseFactory())->createResponse();
+            });
+
+        return $middleware->process($request, $handler);
+    }
+
+    public function testUploadedFiles(): void
+    {
+        $middleware = new ClearEmptyInput();
+
+        $realFile = (new UploadedFileFactory())->createUploadedFile(
+            (new StreamFactory())->createStream(''),
+            0,
+            UPLOAD_ERR_OK,
+        );
+
+
+
+        $noFile = (new UploadedFileFactory())->createUploadedFile(
+            (new StreamFactory())->createStream(''),
+            0,
+            UPLOAD_ERR_NO_FILE,
+        );
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://test')
+            ->withUploadedFiles([
+                'real_file' => $realFile,
+                'none_file' => $noFile,
+                'files' => [$realFile, $noFile],
+            ]);
+
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $handler->expects(self::once())->method('handle')
+            ->willReturnCallback(static function (ServerRequestInterface $request) use ($realFile): ResponseInterface {
+                self::assertEquals([
+                    'real_file' => $realFile,
+                    'files' => [$realFile],
+                ], $request->getUploadedFiles());
+                return (new ResponseFactory())->createResponse();
+            });
+
+        $middleware->process($request, $handler);
+    }
+}
