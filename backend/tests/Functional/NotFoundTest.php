d6c847162842126015bff792d81f8790a406978e
Alexey Shmelev <alexsey_89@bk.ru>
2024-02-23 19:56:21 +0300

    fix not found test

--- backend/tests/Functional/NotFoundTest.php
+++ backend/tests/Functional/NotFoundTest.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Test\Functional;
 
-
-
-
 final class NotFoundTest extends WebTestCase
 {
     public function testNotFound(): void
@@ -14,7 +11,7 @@ final class NotFoundTest extends WebTestCase
         $response = $this->application()->handle(self::json('GET', '/not-found'));
 
         self::assertEquals(404, $response->getStatusCode());
-        self::assertJson($body = (string)$response->getBody());
+        self::assertJson($body = (string) $response->getBody());
 
         $data = Json::decode($body);
 
