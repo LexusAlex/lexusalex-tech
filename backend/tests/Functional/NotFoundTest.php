f99c014b05d4874a06b091ee6c7f9b6782265c92
Alexey Shmelev <alexsey_89@bk.ru>
2024-02-23 19:54:49 +0300

    added not found test

---
+++ backend/tests/Functional/NotFoundTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+
+
+
+final class NotFoundTest extends WebTestCase
+{
+    public function testNotFound(): void
+    {
+        $response = $this->application()->handle(self::json('GET', '/not-found'));
+
+        self::assertEquals(404, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
+
+        $data = Json::decode($body);
+
+        self::assertEquals('404 Not Found', $data['message']);
+    }
+}
