6e2bde3f6113d80f6e28cb9a40017df3e72204f9
Alexey Shmelev <alexsey_89@bk.ru>
2024-02-26 22:37:23 +0300

    added next auth

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,6 +2,7 @@ import "@/assets/css/styles.scss";
 import {Montserrat} from "next/font/google";
 import BootstrapClient from '@/components/BootstrapClient/BootstrapClient.jsx';
 import App from "@/components/App/App";
+import {AppSessionProvider} from "@/session/app-session-provider";
 
 const Font = Montserrat({ subsets: ['cyrillic-ext','latin','cyrillic'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={Font.className}>
+        <AppSessionProvider />
         <App>{children}</App>
         <BootstrapClient />
       </body>
