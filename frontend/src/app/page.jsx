15daa6fa08ee7ce7a90f674855313d6cf882696e
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-01 16:28:26 +0300

    remove auth and update deps next

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,7 @@
-import { options } from "./api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
-import Link from "next/link";
-
 export default async function Home() {
 
   return (
       <main>
-        <Link href="/api/auth/signin">Sign In</Link>
         <div className="text-center mt-4 col-md-6 mx-auto">
           <h1 className="text-danger">Hello Bootstrap</h1>
 
