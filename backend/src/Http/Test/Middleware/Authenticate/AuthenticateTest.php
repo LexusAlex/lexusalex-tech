fae8c711bdfa42b108ed3433a9723ab6dcd8c731
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-11 21:41:46 +0300

    write authenticate test

--- backend/src/Http/Test/Middleware/Authenticate/AuthenticateTest.php
+++ backend/src/Http/Test/Middleware/Authenticate/AuthenticateTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Test\Middleware\Authenticate;
 
 use App\Http\Middleware\Authenticate\Authenticate;
-use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\ResourceServer;
 use LogicException;
 use PHPUnit\Framework\TestCase;
@@ -74,7 +73,6 @@ final class AuthenticateTest extends TestCase
 
     public function testNotValidRequest(): void
     {
-        $header = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJmcm9udGVuZCIsImp0aSI6ImQzMDA1Y2ZhOGE0ODU3YTE1NzViNGU2YzYwM2U0YWIxNzkxMDdjN2FmMGFmMDVjYTg0NDgxY2E4MjZlYzcxNzlmNDYwOTc5NDlkMzBlMTM0IiwiaWF0IjoxNzEwMDYwMzUzLjE5NTk4NiwibmJmIjoxNzEwMDYwMzUzLjE5NTk5MiwiZXhwIjozMzI2Njk2OTE1My4xNzMwNCwic3ViIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAxIiwic2NvcGVzIjpbImNvbW1vbiJdfQ.J3fgYuV3CtudqF8gxMvbbFAeVQNn1Z0HNJPtcXZ01jzvam13RprU2XtrS5ntf0MjB5AJymPIALX5Yjyo_OJ0LaRt82MzSLWtP7BT9Wvfbp_qdOsN7rTNiWZW3pUlkt9sQd5NzFA_ECBwhT-yuFD95rCYJWHHac22YTZb0YOSVHNOrxLaaQtbEPePtRgUKKrdgHWUdY8Ga5ahJ27TvHRdo1iIM1CFidyxSgGGcQDMc-d_SaiJvM02st-X2Zm112doA35NJ2DD8Eep3_efmqCSZjOy-NWl6dU8yJjTVJkZQU9WsdQ0iSle-vBaNMWBi77muyzREIABWJew6DIxII3qdQ';
         $logger = $this->createMock(LoggerInterface::class);
 
         $dependencies = (require __DIR__ . '/../../../../Configurations/Main/dependencies.php')((__DIR__ . '/../../../../../src'));
@@ -83,7 +81,7 @@ final class AuthenticateTest extends TestCase
 
         $server = $container->get(ResourceServer::class);
 
-        $response = $this->createMock(ResponseFactoryInterface::class);
+        $response = $container->get(ResponseFactoryInterface::class);
 
         $middleware = new Authenticate($server, $response, $logger);
 
@@ -92,9 +90,10 @@ final class AuthenticateTest extends TestCase
 
         $request = (new ServerRequestFactory())
             ->createServerRequest('POST', 'http://test')
-            ->withHeader('Authorization', $header);
+            ->withHeader('Authorization', '1');
 
-        //$this->expectException(OAuthServerException::class);
-        //$middleware->process($request, $handler);
+        $r = $middleware->process($request, $handler);
+        self::assertEquals($r->getStatusCode(), 401);
+
     }
 }
