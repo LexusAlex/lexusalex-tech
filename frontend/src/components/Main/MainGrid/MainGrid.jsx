e2b6ebe9d6eb20b80c13656d441822e21502086f
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-30 00:41:19 +0300

    fixes

--- frontend/src/components/Main/MainGrid/MainGrid.jsx
+++ frontend/src/components/Main/MainGrid/MainGrid.jsx
@@ -1,8 +1,8 @@
-import {Box, Grid, GridItem, SimpleGrid, Skeleton, Stack, Text} from "@chakra-ui/react";
+import {Box, Grid, GridItem, Image, SimpleGrid, Skeleton, Stack, Text} from "@chakra-ui/react";
 
 export default function MainGrid({ children })
 {
-    /*
+    
     return (
         <>
             <Grid
@@ -10,17 +10,23 @@ export default function MainGrid({ children })
                 gridTemplateRows={"50px auto 50px"}
                 minH={'100vh'}
                 gap={'5px'}
-                gridTemplateAreas={
-                    `"h h h h h h h h h h h h"
+                gridTemplateAreas={{
+                    base: `"h h h h h h h h h h h h"
                     "menu menu m m m m m m m m m m"
+                    "f f f f f f f f f f f f"`,
+                    md: `"h h h h h h h h h h h h"
+                    "menu menu m m m m m m m m m m"
+                    "f f f f f f f f f f f f"`,
+                    sm: `"h h h h h h h h h h h h"
+                    "m m m m m m m m m m m m"
                     "f f f f f f f f f f f f"`
-                }
+                }}
 
             >
                 <GridItem area={'h'} border={'solid'} bg={'green.50'}>
                     Header
                 </GridItem>
-                <GridItem area={'menu'} border={'solid'}>
+                <GridItem area={'menu'} border={'solid'} hideFrom={'sm'}>
                     Menu
                 </GridItem>
                 <GridItem area={'m'} border={'solid'}>
@@ -32,15 +38,40 @@ export default function MainGrid({ children })
             </Grid>
         </>
     )
-     */
 
+
+    /*
     return (
-    <SimpleGrid columns={{ base: 12, md: 1, lg: 3 }} gap={5}>
+    <SimpleGrid
+        columns={{ base: 3,sm: 2, md: 1, lg: 1, xl: 1, "2xl": 1}}
+        spacing={3}
+        minH={'100vh'}
+    >
         <Box bg={'green.400'}>
             header
         </Box>
+        <Box bg={'green.400'}>
+            aside
+        </Box>
+        <Box bg={'green.400'}>
+            main
+        </Box>
+        <Box bg={'green.400'}>
+            footer
+        </Box>
+        <SimpleGrid
+            minChildWidth="150px"
+            spacing="40px"
+            maxW="xl"
+            alignItems="center"
+            justifyContent="center"
+            margin="100px auto"
+        >
+        </SimpleGrid>
     </SimpleGrid>
     )
+
+     */
 
 
 }
