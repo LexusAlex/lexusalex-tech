7aec234930fc02fb5da3b32d4d67d6d6c4d48759
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-29 16:54:49 +0300

    added new grid layout

--- frontend/src/components/Main/MainGrid/MainGrid.jsx
+++ frontend/src/components/Main/MainGrid/MainGrid.jsx
@@ -1,32 +1,34 @@
-import {Box, Grid, GridItem, SimpleGrid} from "@chakra-ui/react";
+import {Box, Grid, GridItem, SimpleGrid, Skeleton, Stack} from "@chakra-ui/react";
 
 export default function MainGrid({ children })
 {
     return (
-        <Grid
-            templateAreas={`"header header"
-                  "nav main"
-                  "nav footer"`}
-            gridTemplateRows={'70px 1fr 30px'}
-            gridTemplateColumns={'150px 1fr'}
-            h='100vh'
-            w='100vw'
-            gap='2'
-            color='blackAlpha.700'
-            fontWeight='bold'
-        >
-            <GridItem pl='2' bg='beige' area={'header'}>
-                Header
-            </GridItem>
-            <GridItem pl='2' bg='beige' area={'nav'}>
-                Nav
-            </GridItem>
-            <GridItem pl='2' bg='beige' area={'main'}>
-                {children}
-            </GridItem>
-            <GridItem pl='2' bg='beige' area={'footer'}>
-                Footer
-            </GridItem>
-        </Grid>
+        <>
+            <Grid
+                gridTemplateColumns={"repeat(12, 1fr)"}
+                gridTemplateRows={"50px auto 50px"}
+                minH={'100vh'}
+                gap={'5px'}
+                gridTemplateAreas={
+                    `"h h h h h h h h h h h h"
+                    "menu menu m m m m m m m m m m"
+                    "f f f f f f f f f f f f"`
+                }
+
+            >
+                <GridItem area={'h'} border={'solid'} bg={'green.50'}>
+                    Header
+                </GridItem>
+                <GridItem area={'menu'} border={'solid'}>
+                    Menu
+                </GridItem>
+                <GridItem area={'m'} border={'solid'}>
+                    {children}
+                </GridItem>
+                <GridItem area={'f'} border={'solid'}>
+                    Footer
+                </GridItem>
+            </Grid>
+        </>
     )
 }
