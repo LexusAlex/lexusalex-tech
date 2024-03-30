3b4222bf3d72de6998e373655fdc3eb833ff43e5
Alexey Shmelev <alexsey_89@bk.ru>
2024-03-31 01:14:31 +0300

    write frontend

--- frontend/src/components/Main/Header/Header.jsx
+++ frontend/src/components/Main/Header/Header.jsx
@@ -7,20 +7,34 @@ import {
     Container,
     Flex,
     Heading,
-    HStack, Icon, SimpleGrid,
+    HStack, Icon, Link, SimpleGrid,
     Spacer, Square,
     Stack,
     Text, Wrap, WrapItem
 } from "@chakra-ui/react";
-import {FaDiceD20} from "react-icons/fa6";
+import {FaSquarespace} from "react-icons/fa6";
+import NextLink from 'next/link'
 
 export default function Header() {
     return (
         <>
             <Container maxW={{ base: "85%"}}>
-                <SimpleGrid gap={'5px'} gridTemplateColumns={"repeat(10, 1fr)"} gridTemplateRows={"5.5rem"}>
+                <SimpleGrid gap={'5px'} gridTemplateColumns={"repeat(10, 1fr)"} gridTemplateRows={"4rem"}>
+                    <Box margin={'auto'} mr={"9px"}>
+                        <FaSquarespace color={'green'} size={'50'} fontSize='1.5rem' />
+                    </Box>
+                    <Box margin={'auto'} ml={'0'}>
+                        <Text fontWeight={'bold'} color={'green.700'}>Tech</Text>
+                    </Box>
+                    <Box></Box>
+                    <Box></Box>
+                    <Box></Box>
+                    <Box></Box>
+                    <Box></Box>
+                    <Box></Box>
+                    <Box></Box>
                     <Box margin={'auto'}>
-                        <FaDiceD20 color={'green'} size={'50'} fontSize='1.5rem' />
+                        <Button colorScheme={"teal"} as={NextLink} href="/login">Login</Button>
                     </Box>
                 </SimpleGrid>
             </Container>
