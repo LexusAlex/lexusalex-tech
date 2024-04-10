5a854f9168340cab05655c0c1d3a82510656c5e5
Alexey Shmelev <alexsey_89@bk.ru>
2024-04-10 20:37:58 +0300

    update deps and reformat components

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,7 +2,8 @@ import "@/assets/css/styles.scss";
 import {Montserrat} from "next/font/google";
 import AuthProvider from "@/components/OAuth/Provider/AuthProvider";
 import { Providers } from './providers'
-import Application from "@/components/Main/Application/Application";
+import {Grid, GridItem} from "@chakra-ui/react";
+import Header from "@/components/Main/Header/Header";
 
 const Font = Montserrat({ subsets: ['cyrillic-ext','latin','cyrillic'] })
 
@@ -17,7 +18,43 @@ export default function RootLayout({ children }) {
       <body className={Font.className}>
         <AuthProvider></AuthProvider>
         <Providers>
-            <Application>{children}</Application>
+            <Grid
+                gridTemplateColumns={"repeat(12, 1fr)"}
+                gridTemplateRows={"4.5rem auto 4.5rem"}
+                minH={'100vh'}
+                gap={'5px'}
+                gridTemplateAreas={{
+                    base: `"h h h h h h h h h h h h"
+                    ". m m m m m m m m m m ."
+                    "f f f f f f f f f f f f"`,
+                    sm: `"h h h h h h h h h h h h"
+                    ". m m m m m m m m m m ."
+                    "f f f f f f f f f f f f"`,
+                    md: `"h h h h h h h h h h h h"
+                    ". m m m m m m m m m m ."
+                    "f f f f f f f f f f f f"`,
+                    lg: `"h h h h h h h h h h h h"
+                    "menu menu menu m m m m m m m m m"
+                    "f f f f f f f f f f f f"`,
+                    xl: `"h h h h h h h h h h h h"
+                    ". menu menu m m m m m m m m ."
+                    "f f f f f f f f f f f f"`
+                }}
+
+            >
+                <GridItem area={'h'} border={'solid'} >
+                    <Header></Header>
+                </GridItem>
+                <GridItem area={'menu'} border={'solid'} hideBelow={'lg'}>
+                    Menu
+                </GridItem>
+                <GridItem area={'m'} border={'solid'}>
+                    {children}
+                </GridItem>
+                <GridItem area={'f'} border={'solid'}>
+                    Footer
+                </GridItem>
+            </Grid>
         </Providers>
       </body>
     </html>
--- frontend/src/components/Main/Application/Application.jsx
+++
@@ -1,9 +0,0 @@
-import MainGrid from "@/components/Main/MainGrid/MainGrid";
-
-export default function Application({ children }) {
-    return (
-        <>
-            <MainGrid>{children}</MainGrid>
-        </>
-    );
-}
